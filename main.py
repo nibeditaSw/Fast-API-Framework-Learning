44ddcd887faae6947f792e5d1fb4a2c9151a5945
nibedita.swain <nibedita.swain@annalect.com>
2024-09-10 12:17:24 +0530

    added put route & sort based on id column, order in asc & desc

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from fastapi import HTTPException, Path, Body
+from fastapi import HTTPException, Path, Body, Query
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
 from pokemon_load_data import SessionLocal, Pokemon, app
 import uvicorn
+from sqlalchemy import asc, desc
 
 # Define Pydantic models
 class PokemonCreate(BaseModel):
@@ -23,15 +24,38 @@ class PokemonCreate(BaseModel):
 class PokemonResponse(PokemonCreate):
     pokemon_id: int
 
+class PokemonUpdate(BaseModel):
+    name: str
+    type_1: Optional[str] = None
+    type_2: Optional[str] = None
+    total: Optional[int] = None
+    hp: Optional[int] = None
+    attack: Optional[int] = None
+    defense: Optional[int] = None
+    sp_atk: Optional[int] = None
+    sp_def: Optional[int] = None
+    speed: Optional[int] = None
+    generation: Optional[int] = None
+    legendary: Optional[bool] = None
 
-# Endpoints
-@app.get("/pokemon/{number}", response_model=PokemonResponse)
-def read_pokemon(number: int = Path(description="The number of the Pokémon to retrieve")):
+
+@app.get("/pokemon/", response_model=List[PokemonResponse])
+def read_pokemon(
+    sort_by: str = Query(default="pokemon_id", description="Column to sort by", regex="^(pokemon_id)$"),
+    order: str = Query(default="asc", description="Sort order: 'asc' or 'desc'", regex="^(asc|desc)$")
+):
     db = SessionLocal()
-    db_pokemon = db.query(Pokemon).filter(Pokemon.number == number).first()
+
+    if order == "asc":
+        db_pokemon = db.query(Pokemon).order_by(asc(sort_by)).all()
+    else:
+        db_pokemon = db.query(Pokemon).order_by(desc(sort_by)).all()
+
     db.close()
-    if db_pokemon is None:
-        raise HTTPException(status_code=404, detail="Pokémon not found")
+    
+    if not db_pokemon:
+        raise HTTPException(status_code=404, detail="No Pokémon found")
+
     return db_pokemon
 
 @app.delete("/pokemon/{number}", response_model=PokemonResponse)
@@ -77,6 +101,25 @@ def create_pokemon(pokemon: PokemonCreate = Body(...)):
     db.close()
 
     return new_pokemon
+
+
+@app.put("/pokemon/{number}", response_model=PokemonResponse)
+def update_pokemon(number: int, pokemon: PokemonUpdate = Body(...)):
+    db = SessionLocal()
+    db_pokemon = db.query(Pokemon).filter(Pokemon.number == number).first()
+
+    if db_pokemon is None:
+        db.close()
+        raise HTTPException(status_code=404, detail="Pokémon not found")
+
+    for key, value in pokemon.dict(exclude_unset=True).items():
+        setattr(db_pokemon, key, value)
+
+    db.commit()
+    db.refresh(db_pokemon)
+    db.close()
+
+    return db_pokemon
 
 
 
